a85285935be7f7cd9cc4f18eb7a39e2543b7569d
Muhammad Faizan <fb240951@gmail.com>
2026-03-07 00:10:28 +0500

    final project

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Body1 from '../assets/Body.png'
-import Iphone1 from '../assets/iphone14Pro.png'
+import iphone from "../assets/iPhone14Pro.png";
 const Footer = () => (
   <footer className="py-20 bg-[#ff5a52] text-white">
     <div className="max-w-7xl mx-auto px-6 grid lg:grid-cols-2 gap-10 items-center">
@@ -26,8 +26,8 @@ const Footer = () => (
         <img src={Body1}
           className="w-64 hidden sm:block translate-x-10 translate-y-10 rounded-2xl shadow-xl"
           alt="App mockup" />
-        <img src={Iphone1} className="w-72 rounded-3xl shadow-2xl"
-          alt="iPhone mockup" />
+        
+          <img src={iphone} className="w-72 rounded-3xl shadow-2xl" alt="iphone" />
       </div>
     </div>
   </footer>
